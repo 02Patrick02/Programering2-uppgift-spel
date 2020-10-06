244f7b9c08e2e93cb884d4f4dbfd26cfc728a7d1
02Patrick02 <54435353+02Patrick02@users.noreply.github.com>
2020-10-06 11:01:51 +0200

    påbörjat

---
+++ Template/Template/player.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Template
+{
+    class player : playerbase
+    {
+        public player(Texture2D tex) : base(tex) { }
+
+    }
+}
---
+++ Template/Template/playerbase.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Template
+{
+    class playerbase
+    {
+        Texture2D texture;
+
+        public void Update()
+        {
+            
+        }
+
+        public playerbase(Texture2D tex)
+        {
+            texture = tex;
+        }
+    }
+}
